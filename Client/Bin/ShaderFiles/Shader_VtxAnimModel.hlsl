dfc67b0ccf4d231220e959a370fa735ae4763828
manmarru <soboru123@gmail.com>
2024-09-08 21:53:41 +0900

    .

---
+++ Client/Bin/ShaderFiles/Shader_VtxAnimModel.hlsl
@@ -0,0 +1,92 @@
+
+/* float2 float3 float4 == vector */
+
+matrix			g_WorldMatrix, g_ViewMatrix, g_ProjMatrix;
+texture2D		g_DiffuseTexture;
+
+/* 뼈행렬들(내 모델 전체의 뼈행렬들(x), 현재 그리는 메시에게 영향을 주는 뼈행렬들(o) */
+matrix			g_BoneMatrices[512];
+
+sampler LinearSampler = sampler_state 
+{
+	Filter = MIN_MAG_MIP_LINEAR;
+	AddressU = wrap;
+	AddressV = wrap;
+};
+
+struct VS_IN
+{
+	float3 vPosition : POSITION;
+	float3 vNormal : NORMAL;
+	float2 vTexcoord : TEXCOORD0;		
+	float3 vTangent : TANGENT;
+
+	uint4  vBlendIndices : BLENDINDEX;
+	float4 vBlendWeights : BLENDWEIGHT;
+};
+
+struct VS_OUT
+{	
+	float4 vPosition : SV_POSITION;
+	float2 vTexcoord : TEXCOORD0;	
+};
+
+VS_OUT VS_MAIN(/*정점*/VS_IN In)
+{
+	VS_OUT		Out = (VS_OUT)0;
+
+	float			fWeightW = 1.f - (In.vBlendWeights.x + In.vBlendWeights.y + In.vBlendWeights.z);
+
+	matrix			BoneMatrix = g_BoneMatrices[In.vBlendIndices.x] * In.vBlendWeights.x +
+		g_BoneMatrices[In.vBlendIndices.y] * In.vBlendWeights.y +
+		g_BoneMatrices[In.vBlendIndices.z] * In.vBlendWeights.z +
+		g_BoneMatrices[In.vBlendIndices.w] * fWeightW;
+		
+	vector			vPosition = mul(vector(In.vPosition, 1.f), BoneMatrix);
+	
+	matrix			matWV, matWVP;
+
+	matWV = mul(g_WorldMatrix, g_ViewMatrix);
+	matWVP = mul(matWV, g_ProjMatrix);
+
+	Out.vPosition = mul(vPosition, matWVP);
+	Out.vTexcoord = In.vTexcoord;
+
+	return Out;
+}
+
+
+struct PS_IN
+{
+	float4 vPosition : SV_POSITION;
+	float2 vTexcoord : TEXCOORD0;	
+};
+
+struct PS_OUT
+{
+	vector vColor : SV_TARGET0;
+};
+
+
+PS_OUT PS_MAIN(PS_IN In)
+{
+	PS_OUT			Out = (PS_OUT)0;
+	
+	Out.vColor = g_DiffuseTexture.Sample(LinearSampler, In.vTexcoord);
+
+	if (0.3f >= Out.vColor.a)
+		discard;
+
+	return Out;
+}
+
+
+
+technique11	DefaultTechnique
+{
+	pass Terrain
+	{
+		VertexShader = compile vs_5_0 VS_MAIN();
+		PixelShader = compile ps_5_0 PS_MAIN();
+	}
+}
